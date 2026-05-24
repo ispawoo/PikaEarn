d465ab376168fa7aea087eea65dd9c28be8aa24f
ispawoo <the.ispawoo@gmail.com>
2026-05-25 00:34:32 +0500

    feat: integrate advertising partner SDK script in layout

--- app/layout.js
+++ app/layout.js
@@ -28,7 +28,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning={true} className={`${outfit.variable} h-full select-none antialiased bg-slate-950 text-slate-100`}>
-      <head />
+      <head>
+        <script src='//libtl.com/sdk.js' data-zone='11052758' data-sdk='show_11052758' defer></script>
+      </head>
       <body suppressHydrationWarning={true} className="min-h-full flex flex-col font-sans overflow-x-hidden bg-slate-950 text-slate-50">
         <TelegramProvider>
           {children}
